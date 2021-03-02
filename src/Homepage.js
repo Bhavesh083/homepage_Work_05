6ef130b01922af7bcfdb88df2cc68756b646baed
Bhavesh083 <19113083@student.hindustanuniv.ac.in>
2021-03-02 11:54:53 +0530

    inter font added

--- src/Homepage.js
+++ src/Homepage.js
@@ -3,6 +3,7 @@ import './styles/one.css';
 import './styles/two.css';
 import './styles/mq.css';
 
+
 function Homepage() { 
     return (                    
 
@@ -14,16 +15,17 @@ function Homepage() {
                            <div _ngcontent-xyu-c34="" className="header">
                                <div _ngcontent-xyu-c34="" className="header-formatter">
                                    <button _ngcontent-xyu-c34="" className="header--logo">
-                                       <img _ngcontent-xyu-c34="" src={'/static/images/logo.svg'} style={{ marginRight : '12px'}}  alt="B" /> Be Founders 
+                                       <img _ngcontent-xyu-c34="" src='https://raw.githubusercontent.com/Bhavesh083/homepage_Work_05/3a62cf709d5733ae38f797ac1df6b174a637cd20/public/static/images/logo.svg' style={{ marginRight : '12px'}}  alt="B" /> Be Founders 
                                     </button>
                                     <div _ngcontent-xyu-c34="" className="header-formatter--right">
                                        <div _ngcontent-xyu-c34="" className="header-container--login">
+                                           <button _ngcontent-xyu-c34="" className="header-container__item">Network</button>
                                            <button _ngcontent-xyu-c34="" className="header-container__item--login">Login</button>
                                            <button _ngcontent-xyu-c34="" className="header-container__item">Sign Up</button>
                                        </div>
                                     </div>
                                     <button _ngcontent-xyu-c34="" className="header-container--mobile">
-                                        <img _ngcontent-xyu-c34="" src={'/static/images/hamburger.svg'}  alt="hamburger" />
+                                        <img _ngcontent-xyu-c34="" src='https://raw.githubusercontent.com/Bhavesh083/homepage_Work_05/3a62cf709d5733ae38f797ac1df6b174a637cd20/public/static/images/hamburger.svg'  alt="hamburger" />
                                     </button>
                                 </div>
                             </div>
@@ -41,8 +43,8 @@ function Homepage() {
                                             <button _ngcontent-xyu-c31="" className="landing-page__call-to-action">Join, It's Free!</button>
                                             <div _ngcontent-xyu-c31="" className="landing-page__content-super-script">Join the 1000+ entrepreneurs using Be Founders</div>
                                             </div>
-                                            <img _ngcontent-xyu-c31="" src={'/static/images/forces2.svg'} alt="" className="landing-page-banner__screenshot--web" />
-                                            <img _ngcontent-xyu-c31="" src={'/static/images/forces2.svg'} alt="" className="landing-page-banner__screenshot--mobile" />
+                                            <img _ngcontent-xyu-c31="" src='https://raw.githubusercontent.com/Bhavesh083/homepage_Work_05/3a62cf709d5733ae38f797ac1df6b174a637cd20/public/static/images/forces2.svg' alt="" className="landing-page-banner__screenshot--web" />
+                                            <img _ngcontent-xyu-c31="" src='https://raw.githubusercontent.com/Bhavesh083/homepage_Work_05/3a62cf709d5733ae38f797ac1df6b174a637cd20/public/static/images/forces2.svg' alt="" className="landing-page-banner__screenshot--mobile" />
                                             </div>
                                             <div _ngcontent-xyu-c31="" className="landing-page-banner__row--how-it-works">
                                                     <div _ngcontent-xyu-c31="" className="landing-page-how-it-works" style={{ paddingLeft:'0px'}}  >
@@ -66,28 +68,28 @@ function Homepage() {
                                                 <div _ngcontent-xyu-c31="" className="landing-page__features-title">Vetted Community</div>
                                                 <div _ngcontent-xyu-c31="" className="landing-page__features-body">Members must be approved before they have access to our platform. We do this to ensure our network remains high quality, saving you time.</div>
                                             </div>
-                                            <img _ngcontent-xyu-c31="" className="landing-page__features-img" src={'/static/images/vetted3.svg'} alt="Vetted Community" />
+                                            <img _ngcontent-xyu-c31="" className="landing-page__features-img" src='https://raw.githubusercontent.com/Bhavesh083/homepage_Work_05/3a62cf709d5733ae38f797ac1df6b174a637cd20/public/static/images/vetted3.svg' alt="Vetted Community" />
                                         </div>
                                         <div _ngcontent-xyu-c31="" className="landing-page__features-feature" style={{ flexFlow:'row-reverse wrap' }} >
                                             <div _ngcontent-xyu-c31="" className="landing-page__features-content">
                                                 <div _ngcontent-xyu-c31="" className="landing-page__features-title">Founding Teams Simplified</div>
                                                 <div _ngcontent-xyu-c31="" className="landing-page__features-body">We’ve streamlined the search process by allowing users to sign up in one of the three categories: creative, hacker, or hustler.</div>
                                             </div>
-                                            <img _ngcontent-xyu-c31="" className="landing-page__features-img" src={'/static/images/sec-1.svg'} alt="Founding Teams Simplified" />
+                                            <img _ngcontent-xyu-c31="" className="landing-page__features-img" src='https://raw.githubusercontent.com/Bhavesh083/homepage_Work_05/3a62cf709d5733ae38f797ac1df6b174a637cd20/public/static/images/sec-1.svg' alt="Founding Teams Simplified" />
                                         </div>
                                         <div _ngcontent-xyu-c31="" className="landing-page__features-feature" style={{ flexFlow:'row wrap' }} >
                                             <div _ngcontent-xyu-c31="" className="landing-page__features-content">
                                                 <div _ngcontent-xyu-c31="" className="landing-page__features-title">See What Ideas People Are Working On</div>
                                                 <div _ngcontent-xyu-c31="" className="landing-page__features-body">Enter your concept pitch directly on your profile to attract like-minded individuals.</div>
                                             </div>
-                                            <img _ngcontent-xyu-c31="" className="landing-page__features-img" src={'/static/images/sec-2.svg'} alt="See What Ideas People Are Working On" />
+                                            <img _ngcontent-xyu-c31="" className="landing-page__features-img" src='https://raw.githubusercontent.com/Bhavesh083/homepage_Work_05/3a62cf709d5733ae38f797ac1df6b174a637cd20/public/static/images/sec-2.svg' alt="See What Ideas People Are Working On" />
                                         </div>
                                         <div _ngcontent-xyu-c31="" className="landing-page__features-feature" style={{ flexFlow:'row-reverse wrap' }} >
                                             <div _ngcontent-xyu-c31="" className="landing-page__features-content">
                                                 <div _ngcontent-xyu-c31="" className="landing-page__features-title">Exclusive Deals on Startup Products</div>
                                                 <div _ngcontent-xyu-c31="" className="landing-page__features-body">Save thousands of dollars on products that help you run your startup.</div>
                                             </div>
-                                        <img _ngcontent-xyu-c31="" className="landing-page__features-img" src={'/static/images/deals-7.svg'} alt="Exclusive Deals on Startup Products" />
+                                        <img _ngcontent-xyu-c31="" className="landing-page__features-img" src='https://raw.githubusercontent.com/Bhavesh083/homepage_Work_05/3a62cf709d5733ae38f797ac1df6b174a637cd20/public/static/images/deals-7.svg' alt="Exclusive Deals on Startup Products" />
                                         </div>
                                     </div>    
                                     <div _ngcontent-xyu-c31="" className="landing-page__footer-testimonial">
@@ -106,7 +108,7 @@ function Homepage() {
                                                     
                                                 <div _ngcontent-xyu-c31="" className="landing-page__community-display">
                                                     <div _ngcontent-xyu-c31=""  style={{ display:'flex' , flexFlow: 'row nowrap' }} >
-                                                        <img _ngcontent-xyu-c31="" className="landing-page__community-icon" src={'/static/images/network.svg'} alt="Network" />
+                                                        <img _ngcontent-xyu-c31="" className="landing-page__community-icon" src='https://raw.githubusercontent.com/Bhavesh083/homepage_Work_05/3a62cf709d5733ae38f797ac1df6b174a637cd20/public/static/images/network.svg' alt="Network" />
                                                         <div _ngcontent-xyu-c31="" className="landing-page__community-title">Network</div>
                                                         </div>
                                                         <div _ngcontent-xyu-c31="" className="landing-page__community-body">Find the perfect cofounder to start your business. Filter by roles, skill sets, and more.</div>
@@ -115,7 +117,7 @@ function Homepage() {
                                                         <div _ngcontent-xyu-c31="" className="landing-page__community-divider"></div>
                                                         <div _ngcontent-xyu-c31="" className="landing-page__community-display">
                                                             <div _ngcontent-xyu-c31="" style={{ display:'flex' , flexFlow: 'row nowrap' }}>
-                                                                <img _ngcontent-xyu-c31="" className="landing-page__community-icon" src={'/static/images/rocket.svg'} alt="Resources" />
+                                                                <img _ngcontent-xyu-c31="" className="landing-page__community-icon" src='https://raw.githubusercontent.com/Bhavesh083/homepage_Work_05/3a62cf709d5733ae38f797ac1df6b174a637cd20/public/static/images/rocket.svg' alt="Resources" />
                                                                 <div _ngcontent-xyu-c31="" className="landing-page__community-title">Resources</div>
                                                             </div>
                                                             <div _ngcontent-xyu-c31="" className="landing-page__community-body">We have a resources page dedicated to helping our founders succeed.</div>
@@ -125,7 +127,7 @@ function Homepage() {
                                                         <div _ngcontent-xyu-c31="" className="landing-page__community-row">
                                                             <div _ngcontent-xyu-c31="" className="landing-page__community-display">
                                                                 <div _ngcontent-xyu-c31="" style={{ display:'flex' , flexFlow: 'row nowrap' }}>
-                                                                    <img _ngcontent-xyu-c31="" className="landing-page__community-icon" src={'/static/images/message.svg'} alt="Message" />
+                                                                    <img _ngcontent-xyu-c31="" className="landing-page__community-icon" src='https://raw.githubusercontent.com/Bhavesh083/homepage_Work_05/3a62cf709d5733ae38f797ac1df6b174a637cd20/public/static/images/message.svg' alt="Message" />
                                                                     <div _ngcontent-xyu-c31="" className="landing-page__community-title">Message</div>
                                                                     </div>
                                                                     <div _ngcontent-xyu-c31="" className="landing-page__community-body">Streamline communication by messaging directly on our platform.</div>
@@ -133,7 +135,7 @@ function Homepage() {
                                                                 <div _ngcontent-xyu-c31="" className="landing-page__community-divider"></div>
                                                                 <div _ngcontent-xyu-c31="" className="landing-page__community-display">
                                                                     <div _ngcontent-xyu-c31="" style={{ display:'flex' , flexFlow: 'row nowrap' }} >
-                                                                        <img _ngcontent-xyu-c31="" className="landing-page__community-icon" src={'/static/images/transparency.svg'} alt="Transparency" />
+                                                                        <img _ngcontent-xyu-c31="" className="landing-page__community-icon" src='https://raw.githubusercontent.com/Bhavesh083/homepage_Work_05/3a62cf709d5733ae38f797ac1df6b174a637cd20/public/static/images/transparency.svg' alt="Transparency" />
                                                                         <div _ngcontent-xyu-c31="" className="landing-page__community-title">Transparency</div>
                                                                     </div>
                                                                     <div _ngcontent-xyu-c31="" className="landing-page__community-body">Share startup info directly on your profile, enabling you to make stronger connections.</div>
